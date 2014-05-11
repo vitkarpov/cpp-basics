c5b73a74e28de2cc8b955d00662fb346709949fa
Viktor Karpov <viktor.s.karpov@gmail.com>
2014-05-11 13:47:29 +0400

    adds string length example

---
+++ string-length/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int lengthString( const char *st )
+{
+    int cnt = 0;
+
+    if (st)
+        // поскольку стока всегда заканчивается
+        // нулевым символом, увеличивая указатель на 1,
+        // выйдем из цикла после перебора всей строки
+        while (*st++)
+            ++cnt;
+
+    return cnt;
+}
+
+const char *st = "Количество символов в этой строке";
+
+int main()
+{
+    std::cout << st << ": " << lengthString(st) << "\n";
+    return 0;
+}
